f58b752958330d828d4bc86bd6a3fc4cee5b8de8
Sergey <codename.cobweb@gmail.com>
2020-05-31 12:06:42 +0300

    add render html

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -4,5 +4,18 @@ export class Excel {
 		this.$el = document.querySelector(selector);
 		if(!this.$el) return new Error(`Error: this ${ selector } selector not found.`);
 		this.components = options.components || [];
-	}
+	};
+
+	getRoot(){
+		const $root = document.createElement('div');
+		this.components.forEach( Component => {
+			const component = new Component();
+			$root.insertAdjacentHTML('beforeend', component.toHTML());
+		});
+		return $root; 
+	};
+
+	render(){
+		this.$el.append(this.getRoot());
+	};
 };
